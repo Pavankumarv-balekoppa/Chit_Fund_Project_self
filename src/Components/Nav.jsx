94ba1a76a8964937da0f83c9f5411b51b2c8a815
Pavankumarv-balekoppa <pavankumarv221@gmail.com>
2023-04-23 17:37:10 +0530

    updated

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -5,7 +5,7 @@ const Nav = () => {
     return ( 
         <div className="nav d-flex justify-content-between p-2 bg-primary">
             <div className="logo ms-5">
-                <h3 id="h3" className="text-warning ">LOGO</h3>
+                <h3 id="h3" className="text-warning ">Pavan</h3>
                 {/* <img width={100} height={50} src="" alt="" /> */}
             </div>
             <div className="logout me-5">
